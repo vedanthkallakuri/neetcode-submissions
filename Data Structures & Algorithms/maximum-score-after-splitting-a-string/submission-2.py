3a88441b0964b0cb2577915e7daf12a37973b845
Vedanth Kallakuri <vedanthkallakuri@gmail.com>
2026-07-28 19:31:39 -0400

    Add: maximum-score-after-splitting-a-string - submission-2

---
+++ Data Structures & Algorithms/maximum-score-after-splitting-a-string/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        
+        ones = 0
+        for i in range(len(s)):
+            if s[i] == '1': ones += 1
+        
+        
+        zeros = 0
+        score = 0
+        for i in range(len(s) - 1):
+            if s[i] == '0':
+                zeros += 1
+            else:
+                ones = max(0, ones - 1)
+            score = max(score, zeros + ones)
+
+        return score
